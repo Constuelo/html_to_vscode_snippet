88eaf7920897f444c96265e716dda0ced3d1ffa3
constuelo <ferguson.rory@gmail.com>
2019-04-15 12:18:22 +0100

    Update and complete

--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import os
 from pathlib import Path
 import json
 
-
-snippet_ext = '.json'  # Extension name for sublimes snippet files
 convert_dir = '/convert'  # Location for input files
 
 path = Path(os.path.dirname(__file__))
 content_path = Path(os.path.dirname(__file__) + convert_dir)
-list_path = os.listdir(content_path)
 
 dict_object = dict()
 
+with open('modules.json', 'w') as data:
+    data.write("{\n")
 
-for d, s, f in os.walk(content_path):
-    for file in f:
-        if '.gitignore' not in file:
-            name = file.split(".")[0]
-            html = os.path.join(d, file)
+    for d, s, f in os.walk(content_path):
+        for file in f:
+            if '.gitignore' not in file:
+                name = file.split(".")[0]
+                html = os.path.join(d, file)                
+                
+                with open(html, 'r') as html:
+                    dict_object[name] = html.read()
+                    
+                    data.write('    "{}": {{\n'.format(name))
+                    data.write('        "scope": "html",\n')
+                    data.write('        "prefix": "{}",\n'.format(name))
+                    data.write('        "body": {},\n'.format(json.dumps(dict_object[name])))
+                    data.write('        "description": "{}"\n    }},\n'.format(name))
 
-            with open(html, 'r') as html:
-                dict_object[name] = html.read()
-
-            with open('modules.json', 'w') as data:
-                json.dump(dict_object, data)
+    data.write("\n}")
 
 print(f'All good.')
